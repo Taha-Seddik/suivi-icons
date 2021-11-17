b444ccee8aba9b8190d9fdab888135b96f3788d6
zoubaier chraiet <zouba.chraiet@yahoo.com>
2021-11-17 09:09:31 +0100

    'zz'

---
+++ src/icons/Folder2.tsx
@@ -0,0 +1,25 @@
+import * as React from 'react'
+
+function SvgFolder2(props: React.SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+      viewBox='0 0 24 24'
+      xmlns='http://www.w3.org/2000/svg'
+      width='1em'
+      height='1em'
+      {...props}
+    >
+      <path
+        d='M19 6.94h-6.471a1 1 0 01-.827-.438l-1.405-2.065A1 1 0 009.471 4H5a2 2 0 00-2 2v12a2 2 0 002 2h14a2 2 0 002-2V8.94a2 2 0 00-2-2z'
+        strokeLinecap='round'
+        strokeWidth={1.5}
+        stroke='currentColor'
+        fill='none'
+        strokeLinejoin='round'
+      />
+      <path fill='none' d='M0 0h24v24H0V0z' />
+    </svg>
+  )
+}
+
+export default SvgFolder2
