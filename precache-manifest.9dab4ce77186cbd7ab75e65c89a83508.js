382f9606343fd6de3ac4fc293c15bde5ec3840e5
taha.seddik1992@gmail.com <taha.seddik1992@gmail.com>
2021-12-14 14:38:16 +0100

    Updates

--- precache-manifest.9dab4ce77186cbd7ab75e65c89a83508.js
+++ precache-manifest.9dab4ce77186cbd7ab75e65c89a83508.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "04d98d65454b6059785b1831cf1bb424",
+    "revision": "14b10af3f80c66056e21442d80809752",
     "url": "./index.html"
   },
   {
-    "revision": "1adae8d3d6e475312a7c",
+    "revision": "1649f3de6107ef82f110",
     "url": "./static/css/main.a459f5cb.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "./static/js/2.756e3367.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "1adae8d3d6e475312a7c",
-    "url": "./static/js/main.9c7cfaba.chunk.js"
+    "revision": "1649f3de6107ef82f110",
+    "url": "./static/js/main.34bdd658.chunk.js"
   },
   {
     "revision": "abc5dcdc2ddffaee2b15",
