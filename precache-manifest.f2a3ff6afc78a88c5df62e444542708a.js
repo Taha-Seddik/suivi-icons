b73bc61817626c827bb7e598aba2beb235a88764
taha.seddik1992@gmail.com <taha.seddik1992@gmail.com>
2021-12-15 14:25:01 +0100

    Updates

--- precache-manifest.f2a3ff6afc78a88c5df62e444542708a.js
+++ precache-manifest.f2a3ff6afc78a88c5df62e444542708a.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "14b10af3f80c66056e21442d80809752",
+    "revision": "0b444c5151a766d964879ff789c88b02",
     "url": "./index.html"
   },
   {
-    "revision": "1649f3de6107ef82f110",
+    "revision": "2f2ed6c3ad53efbdb751",
     "url": "./static/css/main.a459f5cb.chunk.css"
   },
   {
-    "revision": "1a3cd47633d9928985b6",
-    "url": "./static/js/2.756e3367.chunk.js"
+    "revision": "8d28e33ee93229bd2d5c",
+    "url": "./static/js/2.9ae5a95d.chunk.js"
   },
   {
     "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
-    "url": "./static/js/2.756e3367.chunk.js.LICENSE.txt"
+    "url": "./static/js/2.9ae5a95d.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "1649f3de6107ef82f110",
-    "url": "./static/js/main.34bdd658.chunk.js"
+    "revision": "2f2ed6c3ad53efbdb751",
+    "url": "./static/js/main.b8f5af36.chunk.js"
   },
   {
     "revision": "abc5dcdc2ddffaee2b15",
